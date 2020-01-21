113112bbe4b428af7ad37cdd93e8aebc6884c2e5
Soutrik Chakraborty <soutrikchakraborty31@gmail.com>
2020-01-22 00:20:23 +0530

    Add files via upload

---
+++ src/main/java/soutrik/whatevervalue/com/oopjava/Boxer.java
@@ -0,0 +1,9 @@
+package soutrik.whatevervalue.com.oopjava;
+
+public class Boxer {
+
+   private int numberofgloves = 2;
+   private int punchspeed = 100;
+   private int punchpower = 200;
+
+}
---
+++ src/main/java/soutrik/whatevervalue/com/oopjava/MainActivity.java
@@ -0,0 +1,21 @@
+package soutrik.whatevervalue.com.oopjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Boxer myBoxer = new Boxer();
+
+
+
+
+
+    }
+}
